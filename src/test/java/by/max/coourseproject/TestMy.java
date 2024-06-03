513da679f4826844ac8a3f4cc023f4933c2ea355
Maksim <karpieichik@bk.ru>
2024-06-03 13:39:12 +0300

    comm

---
+++ src/test/java/by/max/coourseproject/TestMy.java
@@ -0,0 +1,22 @@
+package by.max.coourseproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import javax.xml.namespace.QName;
+
+
+public class TestMy
+{
+    @Test
+    public void CheckName()
+    {
+
+    }
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
